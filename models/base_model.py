3cbcb38b4333040f0355f4804b4e25f4224bc45f
malopro <malovagilbert@gmail.com>
2023-07-13 07:03:36 -0700

    Created base model

---
+++ models/base_model.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+""" This module contains the BaseModel class """
+import uuid
+import datetime
+
+class BaseModel:
+    """ Base model class """
+    
+    def __init__(self):
+        """ Initializes class """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        """ Returns a string representation """
+
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """ updates the public instance attribute updated_at with the current datetime """
+
+        self.updated_at = datetime.datetime.now()
+        self.created_at
+
+    def to_dict(self):
+        """ returns a dictionary containing all keys/values of __dict__ of the instance """
+
+        my_dict = self.__dict__
+        my_dict["__class__"]  = self.__class__.__name__
+
+        self.created_at = datetime.datetime.now().isoformat()
+        self.updated_at = datetime.datetime.now().isoformat()
+
+        return my_dict
+
