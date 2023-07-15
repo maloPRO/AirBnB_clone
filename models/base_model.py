3f5a0aa49af87571b4df026a30c325458fa22a11
malopro <malovagilbert@gmail.com>
2023-07-15 03:27:54 -0700

    Fixed pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -4,9 +4,10 @@
 import uuid
 import datetime
 
+
 class BaseModel:
     """ Base model class """
-    
+
     def __init__(self, *args, **kwargs):
         """ Initializes class """
         if kwargs is not None:
@@ -18,22 +19,30 @@ class BaseModel:
     def __str__(self):
         """ Returns a string representation """
 
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__,
+                self.id,
+                self.__dict__)
 
     def save(self):
-        """ updates the public instance attribute updated_at with the current datetime """
+        """
+        updates the public instance attribute
+        updated_at with the current datetime
+        """
 
         self.updated_at = datetime.datetime.now()
         self.created_at
 
     def to_dict(self):
-        """ returns a dictionary containing all keys/values of __dict__ of the instance """
+        """
+        returns a dictionary containing all
+        keys/values of __dict__ of the instance
+        """
 
         my_dict = self.__dict__
-        my_dict["__class__"]  = self.__class__.__name__
+        my_dict["__class__"] = self.__class__.__name__
 
         self.created_at = datetime.datetime.now().isoformat()
         self.updated_at = datetime.datetime.now().isoformat()
 
         return my_dict
-
