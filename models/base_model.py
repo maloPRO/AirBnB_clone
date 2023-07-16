1a303c7dcceae0f845300327024fa8b54f5154e4
malopro <malovagilbert@gmail.com>
2023-07-16 01:20:40 -0700

    Task 3

--- models/base_model.py
+++ models/base_model.py
@@ -15,7 +15,7 @@ class BaseModel:
                 self.key = value
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
-        self.updated_at = sel.created_at
+        self.updated_at = self.created_at
 
     def __str__(self):
         """ Returns a string representation """
