8933338b17570f1b78e03e0a2f31e9b923631412
malopro <malovagilbert@gmail.com>
2023-07-16 01:31:24 -0700

    Task 3

--- models/base_model.py
+++ models/base_model.py
@@ -10,12 +10,16 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """ Initializes class """
-        if kwargs is not None:
+        if kwargs:
             for key, value in kwargs.items():
-                self.key = value
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """ Returns a string representation """
