0eebbe55e6c523b54ea81a4cc4082640c3912a95
malopro <malovagilbert@gmail.com>
2023-07-16 07:27:37 -0700

    File storage

--- models/base_model.py
+++ models/base_model.py
@@ -25,7 +25,8 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
-                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                        value = datetime.strptime(value,
+                                "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
         else:
             models.storage.new(self)
