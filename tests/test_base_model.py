27e2211d1543df98b89eec812b9791fd40d73906
Chirindo <jessicahchimweri@gmail.com>
2023-07-16 08:04:19 -0700

    basemodel tests

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -2,8 +2,10 @@
 
 """ Base model tests """
 
+import os
 import unittest
 import models
+from time import sleep
 from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
@@ -12,4 +14,15 @@ class TestBaseModel(unittest.TestCase):
         my_model = BaseModel()
         my_model.name = "Model one"
         self.assertEqual(my_model.name, "Model one")
+   
+    def test_differrent_created_at(self):
+        bm1 = BaseModel()
+        sleep(0.1)
+        bm2 = BaseModel()
+        self.assertLess(bm1.created_at, bm2.created_at)
+
+    def test_id(self):
+        bm1 = BaseModel()
+        bm2 = BaseModel()
+        self.assertNotEqual(bm1.id, bm2.id)
 
