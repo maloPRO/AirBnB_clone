2245c996fa611208e20ed1690fec9c1257ac348e
malopro <malovagilbert@gmail.com>
2023-07-17 12:50:16 -0700

    Updated console with new classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,11 @@
 """Defines the FileStorage class."""
 
 import json
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models.user import User
 from models.base_model import BaseModel
 
